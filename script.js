541d350a0968c4354ac50c066b1ed5d6828cbb24
noral1yev17 <asliddinnoraliyev@gmail.com>
2022-10-11 17:30:42 +0500

    homework1

---
+++ script.js
@@ -0,0 +1,61 @@
+"use strict"
+// 1-Misol
+
+// var l=500
+// console.log(l/100+" metr");
+
+
+// 2-Misol
+
+// var m=200
+// console.log(m/1000+" tonna");
+
+
+// 3-Misol
+
+// var a=17
+// var b=7
+// console.log(a/b +" marta joylashtirish mumkin");
+
+// 4-Misol
+
+// var a=56
+// console.log(Math.floor(a/10+" birlik"));  
+// console.log(a%10+" o'nlik");
+
+
+// 5-Misol
+
+// var a = 75
+// var q;
+// var c;
+// // console.log( (q = Math.floor( a / 10)) + " birlik");  
+// // console.log((c = a % 10) + " o'nlik");
+// q = Math.floor( a / 10) ;
+// c =  a % 10;
+// console.log(q+c);
+
+
+// 6-Misol
+
+// var a = 75
+// var q;
+// var c;
+// q = Math.floor( a / 10) ;
+// c =  a % 10;
+// console.log(q*c);
+
+
+// 7-Misol
+
+
+// var a=2022
+// console.log( Math.floor( a / 1000) + " birlik");  
+// console.log(a % 100 + " yuzlik");
+
+
+// 8-Misol
+
+// var a = 360000
+// console.log((Math.floor( a / 3600)) + " soat");
+//  console.log( (Math.floor( a / 60)) + " minut");  
